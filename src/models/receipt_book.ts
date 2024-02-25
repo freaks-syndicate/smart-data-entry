6c63e1aad4ffba15f0869f0d8cdae628a768371d
Atharva-CM <atharva.mahamuni@bluepineapple.io>
2024-02-25 17:45:11 +0530

    feat: added receipt book mongoose model

---
+++ src/models/receipt_book.ts
@@ -0,0 +1,27 @@
+import { model, Schema } from 'mongoose';
+
+import { ReceiptBook } from '../generated/graphql';
+
+const financialYearPattern = /^[0-9]{4}-[0-9]{4}$/;
+
+const ReceiptBookSchema = new Schema<ReceiptBook>({
+  uuid: { type: String, required: true, unique: true },
+  receiptBookNumber: { type: Number, required: true, unique: true },
+  receiptSeries: {
+    type: Number,
+    required: [true, 'Receipt Series is required'],
+    min: [1, 'Receipt Series should be at least 1'],
+  },
+  totalReceipts: {
+    type: Number,
+    required: false,
+    min: [1, 'Total Receipt(s) should be at least 1'],
+  },
+  financialYear: {
+    type: String,
+    required: false,
+    match: [financialYearPattern, 'Please enter a valid financial year (2023-2024)'],
+  },
+});
+
+export const ReceiptBookModel = model('ReceiptBook', ReceiptBookSchema);
